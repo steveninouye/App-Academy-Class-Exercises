2593b9b92918c5f0b1238ca1f58365b339eeb3eb
Steven Inouye <steveninouye@msn.com>
2018-10-29 18:10:24 -0700

    Add corresponding pieces to initial board

--- chess/board.rb
+++ chess/board.rb
@@ -3,30 +3,31 @@ require_relative "display"
 require_relative "pieces/null_piece"
 
 class Board
+  attr_reader :board
 
   def initialize
     @board = create_board
   end
 
   def create_board
-    top = Array.new(2) {Array.new(8) {[Piece.new]}}
-    mid = Array.new(4) {Array.new(8){ [NullPiece.instance] }}
-    bottom = Array.new(2) {Array.new(8) {[Piece.new]}}
+    top = Array.new(2) {Array.new(8) {Piece.new}}
+    mid = Array.new(4) {Array.new(8){ NullPiece.instance }}
+    bottom = Array.new(2) {Array.new(8) {Piece.new}}
     top + mid + bottom
   end
 
   def move(start_pos, end_pos)
-
-
+    begin
       piece = self[start_pos]
       self[end_pos] = piece
     rescue
-
       retry
     end
       self[start_pos] = NullPiece.instance
+  end
 
-
+  def valid_pos?(pos)
+    self[pos]
   end
 
   # Syntactic Sugar for grid assignment for board
