20318390a6cc99c140ca2981fca7379e6364bcf5
Steven Inouye <steveninouye@msn.com>
2018-10-30 16:03:34 -0700

    Add extra movement position allowed to each piece if piece is opponent's piece

--- chess/modules/slideable.rb
+++ chess/modules/slideable.rb
@@ -13,6 +13,7 @@ module SlidingPiece
 
   def horizontal_dirs
     horizontal_moves = []
+    opposing_color = self.color == :white ? :black : :white
 
     row, col = pos
     row += 1
@@ -20,6 +21,7 @@ module SlidingPiece
       horizontal_moves << [row, col]
       row += 1
     end
+    horizontal_moves << [row, col] if row > 7 && @board[row,col].color == opposing_color
 
     row, col = pos
     row -= 1
@@ -27,62 +29,70 @@ module SlidingPiece
       horizontal_moves << [row, col]
       row -= 1
     end
+    horizontal_moves << [row, col] if row < 0 && @board[row,col].color == opposing_color
 
     row, col = pos
     col -= 1
-    until col < 0|| !@board[row,col].is_a(NullPiece)
+    until col < 0 || !@board[row,col].is_a(NullPiece)
       horizontal_moves << [row, col]
       col -= 1
     end
+    horizontal_moves << [row, col] if col < 0 && @board[row,col].color == opposing_color
 
     row, col = pos
     col += 1
-    until col > 7|| !@board[row,col].is_a(NullPiece)
+    until col > 7 || !@board[row,col].is_a(NullPiece)
       horizontal_moves << [row, col]
       col += 1
     end
+    horizontal_moves << [row, col] if col > 7  && @board[row,col].color == opposing_color
 
     horizontal_moves
   end
 
   def diagonal_dirs
     diagonal_moves = []
+    opposing_color = self.color == :white ? :black : :white
 
     row, col = pos
     row += 1
     col += 1
-    until row > 7 || col > 7|| !@board[row,col].is_a(NullPiece)
+    until row > 7 || col > 7 || !@board[row,col].is_a(NullPiece)
       diagonal_moves << [row, col]
       row += 1
       col += 1
     end
+    diagonal_moves << [row, col] if (row > 7 || col > 7)  && @board[row,col].color == opposing_color
 
     row, col = pos
     row -= 1
     col -= 1
-    until row < 0 || col < 0|| !@board[row,col].is_a(NullPiece)
+    until row < 0 || col < 0 || !@board[row,col].is_a(NullPiece)
       diagonal_moves << [row, col]
       row -= 1
       col -= 1
     end
+    diagonal_moves << [row, col] if (row < 0 || col < 0)  && @board[row,col].color == opposing_color
 
     row, col = pos
     row += 1
     col -= 1
-    until row > 7 || col < 0|| !@board[row,col].is_a(NullPiece)
+    until row > 7 || col < 0 || !@board[row,col].is_a(NullPiece)
       diagonal_moves << [row, col]
       row += 1
       col -= 1
     end
+    diagonal_moves << [row, col] if (row > 7 || col < 0)  && @board[row,col].color == opposing_color
 
     row, col = pos
     row -= 1
     col += 1
-    until row < 0 || col > 7|| !@board[row,col].is_a(NullPiece)
+    until row < 0 || col > 7 || !@board[row,col].is_a(NullPiece)
       diagonal_moves << [row, col]
       row -= 1
       col += 1
     end
+    diagonal_moves << [row, col] if (row < 0 || col > 7)  && @board[row,col].color == opposing_color
     diagonal_moves
   end
 end
